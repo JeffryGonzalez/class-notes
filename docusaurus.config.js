a0d6bc241fdf0e13aea7b0b8783fce57fd39ec2b
Jeffry Gonzalez <jeff@hypertheory.com>
2022-09-12 15:04:52 -0400

    Some Notes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/JeffryGonzalez/class-notes',
             label: 'GitHub',
             position: 'right',
           },
